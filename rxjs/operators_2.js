cc4d8649679f4e4eff57a7e39bb06722e3944fa6
apontes77 <alexandrepontesgyn@gmail.com>
2020-07-16 11:13:16 -0300

    obtendo dados de API externa com Operators

---
+++ rxjs/operators_2.js
@@ -0,0 +1,15 @@
+const { ajax } = require('rxjs/ajax')
+const { XMLHttpRequest } = require('xmlhttprequest')
+const { map, concatAll} = require('rxjs/operators')
+
+
+ajax({
+    createXHR: () => new XMLHttpRequest(),
+    url: 'https://api.github.com/users/apontes77/repos'
+})
+    .pipe(
+        map(resp => JSON.parse(resp.xhr.responseText)),
+        concatAll(),
+        map(repo => repo.full_name)
+    )
+    .subscribe(console.log)
